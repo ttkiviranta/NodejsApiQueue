813d90b6ba56fae4378a028cb459f2d3a2019221
ttkiviranta <ttkiviranta@gmail.com>
2019-06-03 10:01:00 +0300

    README.md

--- NodejsApiQueue/accenture.js
+++
@@ -1,7 +0,0 @@
-var str = "Kalle Mikko Lasse Janne Aku Ike";
-var res = str.split(" "); 
-console.log(res);
-var resLength = res.length;
-for (var i = 0; i < resLength; i++) {
-    console.log(res[i]);
-    }
